b72de64e0a91d12467cd9a333a6f85fa2384fee4
mike_neck <jkrt3333@gmail.com>
2018-04-19 22:32:25 +0900

    IssueId と ProjectId を定義した

---
+++ youtrack4j-core/src/main/java/org/mikeneck/youtrack/api/project/ProjectId.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 Shinya Mochida
+ *
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mikeneck.youtrack.api.project;
+
+public class ProjectId {
+
+  private final String value;
+
+  public ProjectId(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ProjectId)) return false;
+
+    ProjectId projectId = (ProjectId) o;
+
+    return value.equals(projectId.value);
+  }
+
+  @Override
+  public int hashCode() {
+    return value.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    //noinspection StringBufferReplaceableByString
+    final StringBuilder sb = new StringBuilder("ProjectId{");
+    sb.append("value='").append(value).append('\'');
+    sb.append('}');
+    return sb.toString();
+  }
+}
---
+++ youtrack4j-core/src/test/java/org/mikeneck/youtrack/api/issue/IssueIdByLocationTest.java
@@ -0,0 +1,33 @@
+package org.mikeneck.youtrack.api.issue;
+
+import org.junit.jupiter.api.Test;
+import org.mikeneck.youtrack.api.project.ProjectId;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IssueIdByLocationTest {
+
+  private final String location = "https://test.example.com/youtrack/rest/issue/TEST-200";
+
+  private final IssueId issueId = new IssueIdByLocation(location);
+
+  @Test
+  void numberReturns200() {
+    assertThat(issueId.number()).isEqualTo(200);
+  }
+
+  @Test
+  void projectIdReturnsTEST() {
+    assertThat(issueId.projectId()).isEqualTo(new ProjectId("TEST"));
+  }
+
+  @Test
+  void resourceUriReturnsOriginalLocation() {
+    assertThat(issueId.resourceUri()).isEqualTo(location);
+  }
+
+  @Test
+  void issuIdReturnsTEST200() {
+    assertThat(issueId.issueId()).isEqualTo("TEST-200");
+  }
+}
